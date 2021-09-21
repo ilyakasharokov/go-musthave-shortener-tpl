3bc0ca253611b4e599302cdf5f37a147e56323c4
ikasharokov <ikasharokov>
2021-09-21 15:17:38 +0300

    inc 7 fixes

--- internal/app/handlers/handlers_test.go
+++ internal/app/handlers/handlers_test.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"fmt"
 	"github.com/stretchr/testify/assert"
+	"ilyakasharokov/cmd/shortener/configuration"
 	"ilyakasharokov/internal/app/model"
 	"ilyakasharokov/internal/app/repository"
 	"net/http"
@@ -12,6 +13,12 @@ import (
 )
 
 const testURL = "https://yandex.ru"
+
+var cfg = configuration.Config{
+	ServerAddress:   "http://localhost:8080",
+	BaseURL:         "http://example.com",
+	FileStoragePath: "",
+}
 
 func TestCreateShort(t *testing.T) {
 	type want struct {
@@ -48,7 +55,8 @@ func TestCreateShort(t *testing.T) {
 			},
 		},
 	}
-	repo := repository.New()
+
+	repo := repository.New(cfg)
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -96,7 +104,7 @@ func TestGetShort(t *testing.T) {
 		},
 	}
 
-	repo := repository.New()
+	repo := repository.New(cfg)
 	repo.AddItem(testCode, model.Link{URL: testURL})
 
 	for _, tt := range tests {
@@ -109,6 +117,61 @@ func TestGetShort(t *testing.T) {
 			defer res.Body.Close()
 			//status code
 			assert.EqualValues(t, tt.want.code, res.StatusCode)
+		})
+	}
+}
+
+func TestAPICreateShort(t *testing.T) {
+	type want struct {
+		code        int
+		contentType string
+	}
+	tests := []struct {
+		name    string
+		payload string
+		want    want
+	}{
+		{
+			name:    "#1 post request test good payload",
+			payload: testURL,
+			want: want{
+				code:        http.StatusCreated,
+				contentType: "text/plain; charset=utf-8",
+			},
+		},
+		{
+			name:    "#2 post request test empty payload",
+			payload: "",
+			want: want{
+				code:        http.StatusBadRequest,
+				contentType: "text/plain; charset=utf-8",
+			},
+		},
+		{
+			name:    "#2 post request test not an url",
+			payload: "asdfasfsa",
+			want: want{
+				code:        http.StatusBadRequest,
+				contentType: "text/plain; charset=utf-8",
+			},
+		},
+	}
+
+	repo := repository.New(cfg)
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			request := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(tt.payload))
+			w := httptest.NewRecorder()
+			h := http.HandlerFunc(CreateShort(repo))
+			h.ServeHTTP(w, request)
+			res := w.Result()
+			defer res.Body.Close()
+			//status code
+			assert.EqualValues(t, tt.want.code, res.StatusCode)
+
+			//content-type
+			assert.EqualValues(t, res.Header.Get("Content-Type"), tt.want.contentType)
 		})
 	}
 }
